5bdaa7e132934fe5b69993c89d627ca5e4e2a00e
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-24 16:20:40 -0300

    I initialize tests (forge test)

--- test/CurationManager.t.sol
+++ test/CurationManager.t.sol
@@ -3,23 +3,26 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../contracts/CurationManager.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/token/ERC721/ERC721.sol";
 
 contract ContractTest is Test {
     CurationManager curationManager;
     ERC721 curationPass;
+    string title = "sint mongs";
 
     function setUp() public {
         curationPass = new ERC721("Mint Songs", "MS721");
-        curationManager = new CurationManager(
-            "sint mongs",
-            _curationPass,
-            100,
-            true
-        );
+        curationManager = new CurationManager(title, curationPass, 0, true);
     }
 
-    function testExample() public {
-        assertTrue(true);
+    function testCan_initializeStateVariables() public {
+        assertEq(curationManager.title(), title);
+        assertEq(
+            address(curationManager.curationPass()),
+            address(curationPass)
+        );
+        assertTrue(curationManager.isActive());
+        assertFalse(curationManager.isFinalized());
+        assertEq(curationManager.curationLimit(), 0);
     }
 }
